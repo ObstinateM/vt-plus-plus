4452ee514d5bc2ce20966b12c4910aa13c55328f
Mathis Beauville <mathis.beauville@gmail.com>
2022-10-27 12:44:55 +0200

    refactor: more readable and delete nots-check

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,5 +1,6 @@
 import ical from 'node-ical';
 import 'setimmediate';
+import { getWeekNumber } from './date';
 
 import config from '../assets/config';
 
@@ -19,7 +20,7 @@ export function getEDT(code: string) {
   return new Promise((resolve, reject) => {
     const database: any = [];
 
-    ical.fromURL(config.ical.replaceAll("{{CODE}}", code), {}, function (err, data) {
+    ical.fromURL(config.ical.replaceAll('{{CODE}}', code), {}, function (err, data) {
       if (err) {
         console.log(err);
         reject(err);
@@ -40,25 +41,19 @@ export function getEDT(code: string) {
   });
 }
 
+/**
+ * Return an Object mapped like 'number of the day in a week' -> Array of events for each day
+ * @param {event} edt
+ * @param {number} week
+ */
 export function getWeekEvent(edt: any, week: number) {
   // Some dark magic from stackoverflow
   const date = new Date();
   date.setHours(0, 0, 0, 0);
   date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
-  const week1 = new Date(date.getFullYear(), 0, 4);
 
   const temp = edt
-    .filter(
-      (event: any) =>
-        1 +
-          Math.round(
-            ((event.start.getTime() - week1.getTime()) / 86400000 -
-              3 +
-              ((week1.getDay() + 6) % 7)) /
-              7
-          ) ===
-        week
-    )
+    .filter((event: any) => getWeekNumber(event) === week)
     .reduce(
       (acc: any, event: any) => {
         const day = event.start.getDay();
@@ -83,6 +78,7 @@ export function getCurrentWeekNumber() {
   date.setHours(0, 0, 0, 0);
   date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
   const week1 = new Date(date.getFullYear(), 0, 4);
+
   return (
     1 +
     Math.round(
